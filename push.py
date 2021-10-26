8953ddf1304194f22a6003a3f55727f584877bbd
王炜堃 <wangweikun@fantuan.ca>
2021-10-26 19:43:55 +0800

    fixbug

--- push.py
+++ push.py
@@ -2,7 +2,5 @@ import requests
 import time,sys
 time.sleep(5)
 res = requests.get(url="http://127.0.0.1:16062/ora_service/getsn")
-print(res.text)
-data={"data":res.text}
+data=res.json()
 res = requests.post(url="http://121.41.205.157:3000/push",data=data)
-print(res.text)
